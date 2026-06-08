90ac045065b55a3e86d13bb79c2eff9ee221b51a
Vicente Oscar Claros Mamani <vicenteclaros.system.dev@gmail.com>
2026-06-08 18:15:02 -0400

    Instala Breeze React e Inertia para INTELECTA

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Services\CacheService;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
-Route::get('/test-cache', function (CacheService $cache) {
 
-    $key = 'intelecta:usuarios:total';
-    $data = $cache->get($key, function () {
-        return rand(1, 1000);
-    }, 60);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    return [
-        'key_usada' => $key,
-        'valor' => $data,
-        'timestamp' => now()
-    ];
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
